e07cb47369370642eebdd94fecf7bdc1f35bce8a
Sabrina Chen <sabrinaxchen2016@gmail.com>
2023-02-27 16:41:27 -0800

    updated DateField in the database

--- trips/models.py
+++ trips/models.py
@@ -3,6 +3,7 @@ from django.db import models
 class Trips(models.Model):
     country=models.CharField(max_length=100)
     city=models.CharField(max_length=100)
-    dates=models.CharField(max_length=100)
+    From=models.DateField()
+    To=models.DateField()
     notes=models.CharField(max_length=1000)
     

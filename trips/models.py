3a98a9faf7bbe3d383ae38a045555637beb6682c
Sabrina Chen <sabrinaxchen2016@gmail.com>
2023-02-28 21:56:09 -0800

    updated database field

--- trips/models.py
+++ trips/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class Trips(models.Model):
     Country=models.CharField(max_length=100)
     City=models.CharField(max_length=100)
-    From=models.DateField(auto_now=True)
-    To=models.DateField(auto_now=True)
+    From=models.DateField(auto_now=False)
+    To=models.DateField(auto_now=False)
     Notes=models.CharField(max_length=1000)
     
